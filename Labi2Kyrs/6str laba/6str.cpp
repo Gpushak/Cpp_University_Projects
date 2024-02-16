7c3302d910dbb586861044280bf51ffc7827a898
Gleb Menov <menovgleb10@gmail.com>
2024-02-16 15:35:36 +0500

    Баба Зина - Любовь на века

--- Labi2Kyrs/6str laba/6str.cpp
+++ Labi2Kyrs/6str laba/6str.cpp
@@ -2,74 +2,69 @@
 #include <string>
 using namespace std;
 
-void separateAndPrintWords(string s) {
+int sortSlov(string s) {
     string digits; // Слова, содержащие только цифры
     string letters; // Слова, содержащие только буквы
     string mixed; // Слова, содержащие и буквы и цифры
 
-    int start = 0;
-    int end = 1;
+    int dig = 0;
+    int lat = 0;
 
-    // Перебираем символы строки
-    while (end != string::npos) {
-        // Находим начало следующего слова
-        start = s.find_first_not_of(" ", end);
+    int end = 0;
+    int bigend = 0;
 
-        // Если достигнут конец строки, выходим из цикла
-        if (start == string::npos)
-            break;
+    int n = size(s);
 
-        // Находим конец текущего слова
-        end = s.find_first_of(".", start);
+    if (n > 255) {
+        cout << "Слово слишком большое" << endl;
+        return 0;
+    }
 
-        // Извлекаем слово из строки
-        string word = s.substr(start, end - start);
+    while (bigend != 1) {
+        dig = 0;
+        lat = 0;
 
-        // Проверяем, содержит ли слово только цифры
-        bool hasDigit = true;
-        for (int i = 0; i < word.size(); i++) {
-            char c = word[i];
-            if (!isdigit(c)) {
-                hasDigit = false;
-                break;
+        end = s.find(" "); //Находим конец слова, если кончается точкой, то эта итерация будет последней.
+        if (end == string::npos) {
+            end = s.find(".");
+            bigend = 1;
+            s.erase(end, 1);
+            s.insert(end, " ");
+        }
+
+        //Определяем содержит ли слово цифры, буквы
+        for (int i = 0; i < end; i++) {
+            if (s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' || s[i] == '5' || s[i] == '6' || s[i] == '7' || s[i] == '8' || s[i] == '9' || s[i] == '0') {
+                dig = 1;
             }
+            else lat = 1;
         }
 
-        // Проверяем, содержит ли слово только буквы
-        bool hasLetter = true;
-        for (int i = 0; i < word.size(); i++) {
-            char c = word[i];
-            if (!isalpha(c)) {
-                hasLetter = false;
-                break;
-            }
+        //Вписываем слово в нужный блок
+        for (int i = 0; i < end; i++) {
+            if (dig == 1 && lat == 0) digits = digits + s[i];
+            else if (dig == 0 && lat == 1) letters = letters + s[i];
+            else if (dig == 1 && lat == 1) mixed = mixed + s[i];
         }
-
-        // Определяем категорию слова и добавляем его в соответствующую строку
-        if (hasDigit && !hasLetter) {
-            digits += word + " ";
-        }
-        else if (!hasDigit && hasLetter) {
-            letters += word + " ";
-        }
-        else {
-            mixed += word + " ";
-        }
+        if (dig == 1 && lat == 0) digits = digits + ' ';
+        else if (dig == 0 && lat == 1) letters = letters + ' ';
+        else if (dig == 1 && lat == 1) mixed = mixed + ' ';
+        s.erase(0, end + 1);
     }
 
     // Выводим слова в заданном порядке
-    cout << digits << letters << mixed << "." << endl;
+    cout << digits << letters << mixed << "." << s << endl;
+    return 1;
 }
 
 int main() {
     setlocale(LC_ALL, "rus");
     string s;
     cout << "Введите строку: ";
-    getline(cin, s); // Используем getline для ввода строки
+    getline(cin, s);
 
-    //Пример показательной строки:  Code Life01 01011010 SetUrMind
-
-    separateAndPrintWords(s);
+    //Пример показательной строки:  Life01 01011010 SetUrMind.
+    sortSlov(s);
 
     return 0;
 }
