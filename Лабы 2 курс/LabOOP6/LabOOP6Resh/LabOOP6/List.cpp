94cd8a1b4088b33745829f7f64934a66787118ca
Gleb Menov <menovgleb10@gmail.com>
2024-04-09 15:28:31 +0500

    heb,jwrgk

--- Лабы 2 курс/LabOOP6/LabOOP6Resh/LabOOP6/List.cpp
+++ Лабы 2 курс/LabOOP6/LabOOP6Resh/LabOOP6/List.cpp
@@ -2,36 +2,8 @@
 #include "List.h"
 using namespace std;
 
-//конструктор с параметрами
-Vector::Vector(int s, int k)
-{
-    size = s;
-    data = new int[size];
-    for (int i = 0;i < size;i++)
-        data[i] = k;
-    beg.elem = &data[0];
-    end.elem = &data[size];
-
-}
-//конструктор копирования
-Vector::Vector(const Vector& a)
-{
-    size = a.size;
-    data = new int[size];
-    for (int i = 0;i < size;i++)
-        data[i] = a.data[i];
-    beg = a.beg;
-    end = a.end;
-
-}
-//деструктор
-Vector::~Vector()
-{
-    delete[]data;
-    data = 0;
-}
 //операция присваивания
-Vector& Vector::operator=(const Vector& a)
+List& List::operator=(const List& a)
 {
     if (this == &a)return *this;
     size = a.size;
@@ -44,34 +16,16 @@ Vector& Vector::operator=(const Vector& a)
     return *this;
 
 }
-//операция доступа по индексу
-int& Vector::operator[](int index)
-{
-    if (index < size) return data[index];
-    else cout << "\nError! Index>size";
-}
-//операция для добавления константы
-Vector Vector::operator+(const int k)//+k
-{
-    Vector temp(size);
-    for (int i = 0;i < size;++i)
-        temp.data[i] += data[i] + k;
-    return temp;
-}
-//операция для получения длины вектора
-int Vector::operator ()()
-{
-    return size;
-}
+
 //операции для ввода-выода
-ostream& operator<<(ostream& out, const Vector& a)
+ostream& operator<<(ostream& out, const List& a)
 {
     for (int i = 0;i < a.size;++i)
         out << a.data[i] << " ";
     return out;
 }
 
-istream& operator>>(istream& in, Vector& a)
+istream& operator>>(istream& in, List& a)
 {
     for (int i = 0;i < a.size;++i)
         in >> a.data[i];
--- Лабы 2 курс/LabOOP6/LabOOP6Resh/LabOOP6/List.h
+++ Лабы 2 курс/LabOOP6/LabOOP6Resh/LabOOP6/List.h
@@ -1,31 +1,59 @@
 #include <iostream>
 using namespace std;
 
-class Vector
+class List
 {
 public:
-    //конструктор с параметрами: выделяет память под s элементов и заполняет их
-    //значением k
-    Vector(int s, int k = 0);
-    //конструктор с параметрами
-    Vector(const Vector& a);
-    //деструктор
-    ~Vector();
+    List() : data(new int[10]), capacity(10), size(0) {}
+
+    ~List() { delete[] data; }
+
+    List(const List& other) : data(new int[other.capacity]), capacity(other.capacity), size(other.size) {
+        for (size_t i = 0; i < size; ++i) {
+            data[i - 1] = other.data[i];
+        }
+    }
+    void resize() {
+        capacity *= 2;
+        int* newData = new int[capacity];
+        for (int i = 0; i < size; ++i) {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+    }
+
+    void push_back(int value) {
+        if (size == capacity) {
+            resize();
+        }
+        data[size++] = value;
+    }
     //оператор присваивания
-    Vector& operator=(const Vector& a);
+    List& operator=(const List& a);
     //операция доступа по индексу
-    int& operator[](int index);
-    //операция для добавление константы
-    Vector operator+(const int k);
-    //операция, возвращающая длину вектора
-    int operator()();
+    int& operator[](size_t index) {
+        if (index >= size) throw std::out_of_range("Index out of range");
+        return data[index];
+    }
+
+    operator int() const { return static_cast<int>(size); }
+
+    List operator+(const List& other) {
+        List result;
+        size_t minSize = std::min(size, other.size);
+        for (size_t i = 0; i < minSize; ++i) {
+            result.push_back(data[i] + other.data[i]);
+        }
+        return result;
+    }
     
     //перегруженные операции ввода-вывода
-    friend ostream& operator<<(ostream& out, const Vector& a);
-    friend istream& operator>>(istream& in, Vector& a);
+    friend ostream& operator<<(ostream& out, const List& a);
+    friend istream& operator>>(istream& in, List& a);
     class Iterator
     {
-        friend class Vector;//дружественный класс
+        friend class List;//дружественный класс
     public:
         Iterator() { elem = 0; }//конструктор без параметров
         Iterator(const Iterator& it) { elem = it.elem; }//конструктор копирования
@@ -45,8 +73,9 @@ public:
     Iterator last() { return end; }//возвращает указатель на элемент следующий за
 
 private:
-    int size;
     int* data;
+    size_t capacity;
+    int size;
     Iterator beg;//указатель на первый элемент вектора
     Iterator end;//указатель на элемент следующий за последним
 
